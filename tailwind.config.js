f9d421ab4b57a79308b6289193566473ac987a5a
Anormalll <gsenturk@mazaka.com.tr>
2026-02-03 16:28:47 +0300

    Tailwind v4ten v3e dusuruldu (fix build error)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', // <--- İŞTE SİHİRLİ AYAR BU
+  darkMode: 'class',
   theme: {
     extend: {},
   },
